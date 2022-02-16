79b93cdd34709d99b6e77ee16758e49b57ce148d
123-code <naranjojose256@gmail.com>
2022-02-16 16:44:47 -0500

    Updated contract files

--- contracts/Tokens.sol
+++ contracts/Tokens.sol
@@ -1,6 +1,47 @@
-pragma solidity ^ 0.8.4;
 
+pragma solidity ^ 0.4.3;
 
 contract Tokens{
+uint max_supply = 1000000000;
+uint current_supply = 0;
+address Address;
+uint balance;
+
+mapping(address=>uint) public balances;
+
+modifier isOwner(){
+    require(msg.sender==Address,"Error"); _;
+}
+
+modifier notSurpass(){
+    require(current_supply<=max_supply,"Error"); _;
+}
+
+modifier singletx(uint amount){
+    require(amount<(max_supply/3),"Error"); _;
+}
+
+modifier cantransfer(){
+    require(balances[msg.sender]>0,"Error");
+
+     _;
+}
+
+function gettokens(uint amount,address receiver)public singletx(amount) {
+    require(amount>0,"Error");
+    balances[receiver]+=amount;
 
 }
+
+function transfer(uint amount,address to) public payable cantransfer() {
+balances[msg.sender] = balances[msg.sender]-amount;   
+balances[to] += (amount);
+
+}
+
+function getbalance(address _Address)  public view returns(uint) {
+    return balances[_Address];
+}
+
+}
+

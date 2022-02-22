026a318197318679f32d22677229e71d50e9f3ef
123-code <naranjojose256@gmail.com>
2022-02-22 17:12:18 -0500

    Updated Membership NFT contract

--- contracts/Membership.sol
+++ contracts/Membership.sol
@@ -1,10 +1,8 @@
-// mint here membership tokens.
-
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^ 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/acess/Ownable.sol";
+import "@openzeppelin/contracts/acess/Ownable.sol"; 
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Minter is ERC721,Ownable{
@@ -18,16 +16,45 @@ constructor() ERC721("Minter","MN"){
 }
 
 mapping(address=>uint)minted;
-mapping(uint=>string)uris;
+mapping(uint=>string)SetUris;
 
-function setUri(uint id,string uri)internal{
-    uris[id] = uri;
+
+struct TokenRender{
+    uint id;
+    string uri;
+
+
 }
 
-function tokenURI(uint token_id) public virtual override returns(string memory){
-string memory t_uri = uris[token_id];
 
+function getalltokens()public view returns(TokenRender[] memory){
+uint latestid = _ntokens.current();
+uint counter = 0;
+TokenRender[] result = new TokenRender[](latestid);
+
+
+for(uint i=0;i<latestid;i++){
+if(_exists(counter)){
+
+string memory uri  = tokenURI(counter);
+res[counter] = TokenRender(counter,uri);
+
+}
+counter++;
+
+}
+return result;
+
+}
+
+function seturi(uint id ,string memory uri)internal{
+SetUris[id] = uri;
+}
+
+function tokenURI(uint token_id )public virtual override returns(string memory){
+string memory t_uri = SetUris[token_id];
 return t_uri;
+
 }
 
 function mint(address recipient, string memory uri ) public returns(uint){ 
@@ -36,8 +63,10 @@ function mint(address recipient, string memory uri ) public returns(uint){
     uint newtoken = _ntokens.current();
     _ntokens.increment();
 _mint(recipient,newtoken);
-    minted[msg.sender] +=1;
-    setUri(newtoken,uri);
+seturi(newtoken,uri);
+    //minted[msg.sender] +=1;
+    return newtoken;
+
 }
 
 

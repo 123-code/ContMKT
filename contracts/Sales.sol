3018ed63582b5cabcb08c6845988007d032d051c
123-code <naranjojose256@gmail.com>
2022-03-02 16:56:13 -0500

    Updated Contract

--- contracts/Sales.sol
+++ contracts/Sales.sol
@@ -1,5 +1,36 @@
+
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^ 0.7.4;
 
-contract Sales{
+
+
+
+contract Sales is Token{
+    address payable Owner;
+
+    mapping(address=>uint)public listings;
+    mapping(address=>string)public vendidos;
+    mapping(address=>string)public comprados;
+    mapping(address=>uint)public numcompras;
+
+
+modifier Price(uint price){
+require(price>0,"Precio No es Válido"); 
+_;
+}
+
+
+
+function post(string name,uint price,string license)public Price(price){ 
+    // falta get info from oracle.
+    
+    listings[msg.sender]=price;
 
 }
+
+function buy(string name,address payable vendor,string license,uint price)public payable{
+comprados[msg.sender] = 
+vendidos[vendor].transfer(price);
+
+}
+} 

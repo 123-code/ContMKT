22f1e07f890de1fa82f6433ad99667f283a3d142
123-code <naranjojose256@gmail.com>
2022-05-31 15:07:21 -0500

    Updated Frontend Files

--- Client/src/Pages/Vender.js
+++ Client/src/Pages/Vender.js
@@ -1,7 +1,6 @@
 import React,{useEffect} from 'react';
 import { ethers } from 'ethers';
-import Governance from 'artifacts/contracts/Governance.sol/Governance.json';
-import Minter from "artifacts/contracts/Minter.sol/Minter.json";
+
 
 const contractaddress = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
 

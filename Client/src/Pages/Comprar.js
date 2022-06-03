a3753d1b9abb53b5b5b57affe4cc712f2ab5a169
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2022-06-03 05:00:46 +0000

    Fixed some frontend bugs

--- Client/src/Pages/Comprar.js
+++ Client/src/Pages/Comprar.js
@@ -1,19 +1,17 @@
 import React,{useEffect} from 'react';
 import { ethers } from 'ethers';
-import Minter from '../artifacts/contracts/Minter.sol/Minter.json';
+import { providers, Contract } from "ethers";
+
 
 // create Compra component
+const provider = new ethers.providers.Web3Provider(window.ethereum)
 const contractaddress = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
-let contract = ethers.Contract(contractaddress,Minter.abi);
+//let contract = ethers.Contract(contractaddress,Minter.abi);
 
-const Comprar = () => {
-   
-    
-    useEffect(() => {})
-    return (
+const Comprar = ()=>{
+    return(
         <>
-            <h1>Comprar</h1>
-
+        <h1> Compra tu auto con nosotros</h1>
         </>
     )
 }

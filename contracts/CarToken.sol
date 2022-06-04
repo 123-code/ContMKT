244e31e605195c605bc7e76fc16a81ce12b9aeaf
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2022-06-04 03:15:51 +0000

    Modified Contract

--- contracts/CarToken.sol
+++ contracts/CarToken.sol
@@ -1,26 +1,26 @@
 pragma solidity ^ 0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
-import "@openzeppelin/contracts/drafts/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 
-contract CarToken is ERC721Full{
+contract CarToken is ERC721{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721Full("CarToken", "CRT")public{
+    constructor() ERC721("CarToken", "CRT")public{
 
     }
 
-    function MintCarToken(address to, string memory uri)public{
+    function MintCarToken(address to, string memory uri)public returns(uint){
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(to,newItemId);
-        _setTokenURI(newItemId, tokenURI);
+       // _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
     }
-
+}
     
 
     
--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -1,9 +1,28 @@
 // SPDX License-Identifier:MIT
 
-pragma solidity ^ 0.8.4;
+pragma solidity ^ 0.8.7;
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
+contract Governance is ERC20Votes {
+   
 
-contract Governance{
-
+    constructor() ERC20("Car-Token","CRT") ERC20Permit("Governance"){
+        // mints one token to whoever can acces the contract.
+        _mint(msg.sender,1);
     }
+// overrides with our own contract values.
+function _afterTokenTransfer(
+address from,
+address to,
+uint amount) internal override(ERC20Votes){
+    super._afterTokenTransfer(from,to,amount);
+}
 
+function _mint(address to,uint amount)internal override(ERC20Votes){
+    super._mint(to,amount);
+}
+
+function _burn(address account,uint amount)internal override(ERC20Votes){
+    super._burn(account,amount);
+}
+}

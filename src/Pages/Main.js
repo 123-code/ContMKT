0b29d2005614e96494a1983a701d65c032dd56ba
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2022-06-12 15:38:39 +0000

    Removed Buttons From Main

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import WalletConnect from '../Components/WalletConnect.jsx';
-import Comprar from './Comprar';
-import Vender from './Vender';
+import form from './Components/Form';
+
 
 
 const Main = () => {
     return (
         <div >
-        <h1>CARZ</h1>
-        <WalletConnect/>
-
-        <div>
-            <h2> Ingresar Como </h2>
-        <form action = "../Pages/Comprar">
-            <button className = "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" > Comprador</button> 
-         </form>
-          
-      <form action='../Pages/Vender'>
-
- 
-          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"> Vendedor </button>
+            <
+       
       </form>
-
-        </div>
 
 
         </div>

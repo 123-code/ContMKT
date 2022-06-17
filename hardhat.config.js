927b348d303a741b6881ff38afc03685b2c8fa7a
123-code <naranjojose256@gmail.com>
2022-06-16 19:02:49 -0500

    Updated Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,7 @@
 require('@nomiclabs/hardhat-waffle');
-const dotenv = require('dotenv').config({path:"./env"});
-const key = process.env.REACTAPP_ALCHEMY_API
+require('dotenv').config({path:"./env"});
 
-console.log(key);
 
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -22,7 +14,7 @@ module.exports = {
       chainId: 1337,
     },
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/-MrMclaQfq5qC4VWybSbIVVuja__LW1N',
+      url: process.env.REACTAPP_ALCHEMY_API,
       accounts: process.env.REACTAPP_PRIVATE_KEY,
     },
   },

925ef9c91c8bc1fe9805199c103366e8c9c16b45
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2022-06-24 23:40:52 +0000

    config files updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,16 @@
-require('@nomiclabs/hardhat-waffle');
-
+require("@nomiclabs/hardhat-waffle");
 require("dotenv").config({ path: ".env" });
 
-const REACTAPP_ALCHEMY_API = process.env.REACTAPP_ALCHEMY_API;
-const REACTAPP_PRIVATE_KEY = process.env.REACTAPP_PRIVATE_KEY;
+const REACTAPP_ALCHEMY_API = process.env.ALCHEMY_API_URL;
+const REACTAPP_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: '0.8.7',
-
+  solidity: "0.8.7",
   networks: {
-    hardhat: {
-      chainId: 1337,
-    },
     rinkeby: {
-      url: REACTAPP_ALCHEMY_API,
-      accounts: [REACTAPP_PRIVATE_KEY],
+      url:ALCHEMY_API_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
     },
   },
 };

8280a3fd06b22d8d9ca40f9007cbaec87aaeeaf4
123-code <naranjojose256@gmail.com>
2022-07-28 23:36:54 -0500

    Added Confirmation Form

--- src/Pages/Confirmation.jsx
+++ src/Pages/Confirmation.jsx
@@ -1,22 +1,38 @@
 import React,{useState} from "react";
+import {Sales,salesabi} from "../../artifacts/contracts/Sales.sol/Sales.json";
 
-let[Confirmed,setConfirmed] = useState(false);
-let[info,setinfo] = useState(' ');
+
+
 
 
 const Confirmation = ()=>{
+
+let[Confirmed,setConfirmed] = useState(false);
+let[info,setinfo] = useState(' ');
+let[buttontext,setbuttontext] = useState('Confirm');
+
+setConfirmed = ()=>{
+    Confirmed = true;
+    setbuttontext('Confirmed');
+
+
+}
+
 return(
     <>
     <h1> Your Payment has been processed!</h1>
 
     <p>
+     
         Your order for { info } has been placed! 
+
+        Once your order arrives, click the button below:
 
 
 
     </p>
 
-    <button> Confirm </button>
+    <button onClick={{setConfirmed}}> {buttontext}</button>
     </>
 )
 }

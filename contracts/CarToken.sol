40cde6efa1823d69e3523ba61acb1bf23cecf890
123-code <naranjojose256@gmail.com>
2022-11-12 12:16:18 -0500

    Contract shouldnt be ownable

--- contracts/CarToken.sol
+++ contracts/CarToken.sol
@@ -3,10 +3,9 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyToken is ERC721, ERC721Burnable, Ownable {
+contract MyToken is ERC721, ERC721Burnable {
     using Counters for Counters.Counter;
 
      Counters.Counter private _tokenIdCounter;
@@ -17,7 +16,7 @@ contract MyToken is ERC721, ERC721Burnable, Ownable {
 
     }
 
-     function safeMint(address to) public  onlyOwner {
+     function safeMint(address to) public   {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);

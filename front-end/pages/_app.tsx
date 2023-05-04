af402144deee748490b789aa895492b3f4696f48
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2023-05-04 05:03:20 +0000

    initial client deps

---
+++ front-end/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { NextUIProvider } from '@nextui-org/react';
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return(
+    <NextUIProvider>
+    <Component {...pageProps} />
+     </NextUIProvider>
+  )
+ 
+  
+}
